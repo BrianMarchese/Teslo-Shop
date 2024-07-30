f931c5a515d27722bfb563de0d888c0f154066ff
BrianMarchese <brianmarchese@hotmail.com>
2024-07-30 11:16:21 -0300

    agregado de sidebar para menu lateral, navegacion en base a las categorias y correcion a la interfaz del producto

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,43 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const seedProducts = initialData.products;
 
 interface Props {
   params: {
-    id: string
+    id: Category;
   }
 }
 
 export default function({ params }: Props) {
   const { id } = params
+  const products = seedProducts.filter( product =>  product.gender === id );
 
-  if (id === "kids"){
-    notFound()
+  const labels: Record<Category, string> = { // hago un tipado de los labels, es de tipo category y apunta a un string
+    'men': 'para Hombres',
+    'women': 'para Mujeres',
+    'kid': 'para Niños',
+    'unisex': 'para Todos'
   }
 
+  const labelsSubtitle: Record<Category, string> = {
+    'men': 'para el',
+    'women': 'para ellas',
+    'kid': 'para los mas chicos',
+    'unisex': 'para todos'
+  }
+
+/*   if (id === "kids"){
+    notFound()
+  } */
+
   return (
-    <div>
-      <h1>Category Page { id }</h1>
-    </div>
+    <>
+    <Title title={`Artículos ${labels[id]}`} subtitle={`Ropa ${labelsSubtitle[id]}`} className="mb-2" />
+
+    <ProductGrid products={ products }/>
+  </>
   );
 }
