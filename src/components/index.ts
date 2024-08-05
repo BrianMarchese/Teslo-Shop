f682a68c747cb562a46d550a4852fc91404032a7
BrianMarchese <brianmarchese@hotmail.com>
2024-08-05 11:05:52 -0300

    maquetado de la pagina de detalle de producto y agregado de carrusel de imagenes

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,9 @@ export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
 export * from './ui/sidebar/SideBar';
 
+export * from './product/size-selector/SizeSelector';
+export * from './product/quantity-selector/QuantitySelector';
+export * from './product/slideshow/ProductSlideShow';
 
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
